72e5863722c5d6fe52ecb14b723059692f136c0f
Leandro Deferrari <68801550+leandrodeferrari@users.noreply.github.com>
2023-01-05 00:45:22 -0300

    Restore project (#2)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ComponentsModule } from './components/components.module';
 
 @NgModule({
   declarations: [
@@ -12,7 +11,6 @@ import { ComponentsModule } from './components/components.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ComponentsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
