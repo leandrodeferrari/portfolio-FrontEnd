51444630f04b0987acf68e98801d26a1fc41f91a
Leandro Deferrari <leandro_deferrari@hotmail.com>
2023-01-12 13:43:25 -0300

    YP-03: Separate components Experience and studies

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -4,13 +4,29 @@ import { AboutMeComponent } from './about-me/about-me.component';
 import { StudiesComponent } from './studies/studies.component';
 import { KnowledgeComponent } from './knowledge/knowledge.component';
 import { ExperiencesComponent } from './experiences/experiences.component';
+import { WorksComponent } from './works/works.component';
+import { WorkComponent } from './work/work.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { ProjectComponent } from './project/project.component';
+import { CareersComponent } from './careers/careers.component';
+import { CareerComponent } from './career/career.component';
+import { CoursesComponent } from './courses/courses.component';
+import { CourseComponent } from './course/course.component';
 
 @NgModule({
   declarations: [
     AboutMeComponent,
     StudiesComponent,
     KnowledgeComponent,
-    ExperiencesComponent
+    ExperiencesComponent,
+    WorksComponent,
+    WorkComponent,
+    ProjectsComponent,
+    ProjectComponent,
+    CareersComponent,
+    CareerComponent,
+    CoursesComponent,
+    CourseComponent
   ],
   imports: [
     CommonModule
